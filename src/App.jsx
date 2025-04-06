5b36b6bc5c1ff0da1bc8796eaeb8062047841f31
Abhi - ROG <ag01portfolio@gmail.com>
2025-04-06 14:10:13 +0530

    createating Board

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+const width = 8; // width of candy
+const candyColors = [
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'yellow'
+]
 function App() {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+
+  // crete a 2d array of 8x8 = 64 random colour
+  const createBoard = () => {
+    const randomColorArrangement = [];
+    for (let i = 0; i < width * width; i++) {
+      const randomNumberFrom0to5 = Math.floor(Math.random() * candyColors.length)
+      const randomColor = candyColors[randomNumberFrom0to5]
+      randomColorArrangement.push(randomColor)
+
+    }
+    setCurrentColorArrangement(randomColorArrangement)
+    console.log("randomColorArrangement:", randomColorArrangement)
+  }
+
+  useEffect(() => {
+    createBoard();
+  }, [])
 
   return (
     <>

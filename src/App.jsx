7f80acb30fdd0e76b036eba860d3acf313b1edd1
Abhi - ROG <ag01portfolio@gmail.com>
2025-04-06 15:17:11 +0530

    check Column 3 or 4

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,19 @@ const candyColors = [
 function App() {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([])
 
+  const checkForColumnOfThree = () => {
+    // 47 is to optimze the performance of the game after last 3rd row there wont be any colum to math 3
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2] // 3 col algo
+      const decidedColor = currentColorArrangement[i]
+
+      // Muru idre tagi brkuuu
+      if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+        columnOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
   // crete a 2d array of 8x8 = 64 random colour
   const createBoard = () => {
     const randomColorArrangement = [];
@@ -28,6 +41,29 @@ function App() {
   useEffect(() => {
     createBoard();
   }, [])
+
+
+  const checkForColumnOfFore = () => {
+    for (let i = 0; i < 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3] // 4 col algo
+      const decidedColor = currentColorArrangement[i]
+
+      if (columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+        columnOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree();
+      checkForColumnOfFore();
+      setCurrentColorArrangement([...currentColorArrangement])
+    }, 100)
+
+    return () => clearInterval(timer);
+  }, [checkForColumnOfThree, currentColorArrangement, checkForColumnOfFore])
+
 
   return (
     <div className='app'>
